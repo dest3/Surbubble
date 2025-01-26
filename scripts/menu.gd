3fad0bc274e5285be763ec626b5f13165dd1ad9c
Matias Aranda <matias_aranda20@hotmail.com>
2025-01-26 15:53:27 -0300

    fix minor bus equisde

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,12 +1,8 @@
 extends Control
-
+#
 func _on_play_pressed() -> void:
 	SceneTransition.change_scene("res://scenes/intro.tscn")
 	print("este boton funciona")
-
-
-func _on_opciones_pressed() -> void:
-	SceneTransition.change_scene("res://scenes/opciones.tscn")
 
 
 func _on_salir_pressed() -> void:
